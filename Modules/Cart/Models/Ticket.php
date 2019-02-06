d8edba54f2929e2fd1b3b46597999fe5c3cb5987
Olimar Ferraz <olimarferraz@gmail.com>
2019-02-06 20:57:31 -0200

    Changed Cart expires_at to W3C timestamp format;

--- Modules/Cart/Models/Ticket.php
+++ Modules/Cart/Models/Ticket.php
@@ -8,10 +8,24 @@ class Ticket extends Model
 {
     public $timestamps = FALSE;
 
-    protected $fillable = ['entrance_id', 'entrance', 'lot', 'price', 'fee', 'name', 'document', 'email'];
+    protected $fillable = [
+        'entrance_id',
+        'entrance',
+        'lot',
+        'price',
+        'fee',
+        'name',
+        'document',
+        'email',
+    ];
 
     protected $casts = [
         'price' => 'integer',
         'fee'   => 'integer',
     ];
+
+    /*public function getPriceAttribute()
+    {
+        return ($this->attributes['value'] + $this->attributes['fee']);
+    }*/
 }
