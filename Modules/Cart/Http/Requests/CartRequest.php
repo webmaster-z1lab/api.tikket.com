d3c4ec362bd83aa86b7b999daff83a057d902179
Olimar Ferraz <olimarferraz@gmail.com>
2019-02-14 17:43:03 -0200

    Created CartRequest customized rules;

--- Modules/Cart/Http/Requests/CartRequest.php
+++ Modules/Cart/Http/Requests/CartRequest.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Cart\Http\Requests;
 
+use Modules\Event\Models\Entrance;
 use Z1lab\JsonApi\Http\Requests\ApiFormRequest;
 
 class CartRequest extends ApiFormRequest
@@ -31,5 +32,33 @@ class CartRequest extends ApiFormRequest
             'tickets.*.quantity' => 'bail|required|integer|min:1',
             'tickets.*.lot'      => 'bail|required|integer|min:1',
         ];
+    }
+
+    /**
+     * @param $validator
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            foreach ($this->request->get('tickets') as $ticket) {
+                $entrance = Entrance::find($ticket['entrance']);
+
+                if ($entrance->available->available < $ticket['quantity']) {
+                    if ($entrance->available->available === 0) {
+                        $validator->errors()->add('tickets', "There are no tickets for entrance '$entrance->name' at the moment.");
+                    } else {
+                        $validator->errors()->add('tickets', "We have only {$entrance->available->available} tickets remaining for the entrance '$entrance->name'");
+                    }
+                } elseif ($ticket['quantity'] > $entrance->max_buy) {
+                    $validator->errors()->add('tickets', "The max tickets for entrance '$entrance->name' is $entrance->max_buy.");
+                } elseif ($ticket['quantity'] < $entrance->min_buy) {
+                    $validator->errors()->add('tickets', "The minimum tickets for entrance '$entrance->name' is $entrance->min_buy.");
+                }
+
+                if (!now()->between($entrance->available->starts_at, $entrance->available->finishes_at)) {
+                    $validator->errors()->add('tickets', "Sales for lot {$entrance->available->lot} are not available.");
+                }
+            }
+        });
     }
 }
