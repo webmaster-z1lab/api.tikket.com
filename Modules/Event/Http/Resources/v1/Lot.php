c463bfea8bee3a69d9607caa00490bbe0b8746c5
Felipe da Silva Pinheiro <felipe.spinheiro88@gmail.com>
2019-02-14 14:02:14 -0200

    Some modifications

--- Modules/Event/Http/Resources/v1/Lot.php
+++ Modules/Event/Http/Resources/v1/Lot.php
@@ -22,8 +22,8 @@ class Lot extends Resource
             'value'       => $this->value,
             'fee'         => $this->fee,
             'price'       => $this->price,
-            'starts_at'   => $this->starts_at,
-            'finishes_at' => $this->finishes_at,
+            'starts_at'   => $this->starts_at->format('d/m/Y'),
+            'finishes_at' => $this->finishes_at->format('d/m/Y'),
         ];
     }
 }
