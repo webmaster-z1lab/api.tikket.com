a11caf5ee1c767cb8007b41ce46d2f773f1cea72
Olimar Ferraz <olimarferraz@gmail.com>
2019-09-02 22:19:15 -0300

    Removed test route;

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
-use App\Mail\Customer\AvailableTicketMail;
-use Modules\Order\Models\Order;
-
 Route::view('/', 'cover');
-
-Route::get('test', function (){
-    return new AvailableTicketMail(Order::first());
-});
