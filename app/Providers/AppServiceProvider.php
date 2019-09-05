5ad9ec5c9e911895f3bd22a9d789ed682cad4670
Olimar Ferraz <webmaster@z1lab.com.br>
2019-09-05 16:04:27 -0300

    Fix in setlocale for date translate config;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        setLocale(LC_TIME, 'pt_BR');
+        setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
         Carbon::setLocale('pt_BR');
 
         $this->app['validator']->resolver(function ($translator, $data, $rules, $messages, $customAttributes) {
